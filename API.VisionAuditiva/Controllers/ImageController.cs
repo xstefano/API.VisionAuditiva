23bb7d6cfa5c50d9a9c86764ddb5dbdf0b4b33c1
xstefano <xcloudsgls@gmail.com>
2023-07-09 01:17:23 -0600

    implementing the ImageController

---
+++ API.VisionAuditiva/Controllers/ImageController.cs
@@ -0,0 +1,21 @@
+﻿using API.VisionAuditiva.Interfaces;
+using API.VisionAuditiva.Message;
+using API.VisionAuditiva.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.VisionAuditiva.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ImageController : ControllerBase
+    {
+        private readonly IImageService _imageService;
+        protected Response _response;
+
+        public ImageController(IImageService imageService)
+        {
+            _imageService = imageService;
+            _response = new Response();
+        }
+    }
+}
