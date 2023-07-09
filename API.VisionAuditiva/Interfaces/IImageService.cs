d803d78e39806a9ae3fb4c9d5c0eba8fe3a7e11b
xstefano <xcloudsgls@gmail.com>
2023-07-09 00:52:29 -0600

    adding IImageService interface

---
+++ API.VisionAuditiva/Interfaces/IImageService.cs
@@ -0,0 +1,11 @@
+﻿using API.VisionAuditiva.Models;
+
+namespace API.VisionAuditiva.Interfaces
+{
+    public interface IImageService
+    {
+        Task<Image> SaveImageFromBase64Async(string base64, string filename);
+        Task<string> GetImageAsBase64Async(string filename);
+        Task<Stream> GetImageStreamAsync(string filename);
+    }
+}
