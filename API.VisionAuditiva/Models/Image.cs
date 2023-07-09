83470d726ebd3eca1e300658394f8de7c671bc9f
xstefano <xcloudsgls@gmail.com>
2023-07-09 00:45:09 -0600

    adding the Image class

---
+++ API.VisionAuditiva/Models/Image.cs
@@ -0,0 +1,8 @@
+﻿namespace API.VisionAuditiva.Models
+{
+    public class Image
+    {
+        public string imageBase64 { get; set; } = string.Empty;
+        public string filename { get; set; } = string.Empty;
+    }
+}
