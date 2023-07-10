f0047394b7254bd42db8ea7cdfee4f02b4e06edd
xstefano <xcloudsgls@gmail.com>
2023-07-09 23:12:55 -0600

    adding the Translation class

---
+++ API.VisionAuditiva/Models/Translation.cs
@@ -0,0 +1,13 @@
+﻿namespace API.VisionAuditiva.Models
+{
+    public class Translation
+    {
+        public string Text { get; set; } = string.Empty;
+        public string To { get; set; } = string.Empty;
+    }
+
+    public class TranslationResponse
+    {
+        public List<Translation> Translations { get; set; } = new List<Translation>();
+    }
+}
