82447f0ee75f3dfdf8ea9ae8a90392cd5a380d0c
xstefano <xcloudsgls@gmail.com>
2023-07-09 23:43:54 -0600

    implementation the CognitiveVisionController

---
+++ API.VisionAuditiva/Controllers/CognitiveVisionController.cs
@@ -0,0 +1,46 @@
+﻿using API.VisionAuditiva.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.VisionAuditiva.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CognitiveVisionController : ControllerBase
+    {
+        private readonly ICognitiveVisionService _cognitiveVisionService;
+
+        public CognitiveVisionController(ICognitiveVisionService service)
+        {
+            _cognitiveVisionService = service;
+        }
+
+        [HttpGet("analyze")]
+        public async Task<IActionResult> AnalyzeFromUrlAsync(string imageUrl)
+        {
+            var description = await _cognitiveVisionService.AnalyzeFromUrlAsync(imageUrl);
+            return Ok(description);
+        }
+        [HttpGet("describe")]
+        public async Task<IActionResult> DescribeFromUrlAsync(string imageUrl)
+        {
+            var description = await _cognitiveVisionService.DescribeFromUrlAsync(imageUrl);
+            return Ok(description);
+        }
+
+
+        [HttpGet("read")]
+        public async Task<IActionResult> ReadImageUrlAsync(string imageUrl)
+        {
+            var text = await _cognitiveVisionService.ReadImageUrlAsync(imageUrl);
+            return Ok(text);
+        }
+
+        [HttpGet("detectObjects")]
+        public async Task<IActionResult> DetectObjectsFromUrlAsync(string imageUrl)
+        {
+            var text = await _cognitiveVisionService.DetectObjectsFromUrlAsync(imageUrl);
+            return Ok(text);
+        }
+    }
+}
