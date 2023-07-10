e4c3cfe07ab11d097ccab77dfb54879d7375caf9
xstefano <xcloudsgls@gmail.com>
2023-07-10 03:35:26 -0600

    first migration database

---
+++ API.VisionAuditiva/Data/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.VisionAuditiva.Data
+{
+    public class ApplicationContext : IdentityDbContext<IdentityUser>
+    {
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            :base(options) { }
+    }
+}
