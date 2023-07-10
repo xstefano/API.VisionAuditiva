b94fac288bfa6599e3e0881a6fcba6e0e3404e18
xstefano <xcloudsgls@gmail.com>
2023-07-09 22:44:35 -0600

    adding the AnalyzeFromUrlAsync method

---
+++ API.VisionAuditiva/Services/CognitiveVisionService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using API.VisionAuditiva.Interfaces;
+
+namespace API.VisionAuditiva.Services
+{
+    public class CognitiveVisionService : ICognitiveVisionService
+    {
+        private readonly ComputerVisionClient _computerVision;
+        private const string endpoint = "[endpoint]";
+        private const string key = "[key]";
+
+        public async Task<string> AnalyzeFromUrlAsync(string imageUrl)
+        {
+            List<VisualFeatureTypes?> features = new List<VisualFeatureTypes?>()
+            {
+                VisualFeatureTypes.Categories, VisualFeatureTypes.Description,
+                VisualFeatureTypes.Faces, VisualFeatureTypes.ImageType,
+                VisualFeatureTypes.Tags, VisualFeatureTypes.Adult,
+                VisualFeatureTypes.Color, VisualFeatureTypes.Brands,
+                VisualFeatureTypes.Objects
+            };
+            ImageAnalysis analysis = await _computerVision.AnalyzeImageAsync(imageUrl, visualFeatures: features, language: "es");
+
+            string? response = analysis.Description.Captions.FirstOrDefault()?.Text;
+
+            if (response != null)
+            {
+                return response;
+            }
+            return "No se ha encontrado ninguna descripcion para la imagen";
+        }
+
+        public Task<string> DescribeFromUrlAsync(string imageUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> DetectObjectsFromUrlAsync(string imageUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> ReadImageUrlAsync(string imageUrl)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
