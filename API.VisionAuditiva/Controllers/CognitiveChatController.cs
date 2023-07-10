1e5a2d6ee318ab50cc0ec7eff24307522080e14a
xstefano <xcloudsgls@gmail.com>
2023-07-10 01:35:00 -0600

    implementing the CognitiveChatController

---
+++ API.VisionAuditiva/Controllers/CognitiveChatController.cs
@@ -0,0 +1,24 @@
+﻿using API.VisionAuditiva.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.VisionAuditiva.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CognitiveChatController : ControllerBase
+    {
+        private readonly ICognitiveChatService _cognitiveChatService;
+
+        public CognitiveChatController(ICognitiveChatService cognitiveChatService)
+        {
+            _cognitiveChatService = cognitiveChatService;
+        }
+
+        [HttpGet("getResponse")]
+        public async Task<IActionResult> getResponse(string request)
+        {
+            var text = await _cognitiveChatService.getResponse(request);
+            return Ok(text);
+        }
+    }
+}
