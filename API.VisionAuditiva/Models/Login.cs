cb6440d4dfb97455f36db9a553b037bc4bb8a427
xstefano <xcloudsgls@gmail.com>
2023-07-10 03:29:41 -0600

    adding the Login class

---
+++ API.VisionAuditiva/Models/Login.cs
@@ -0,0 +1,8 @@
+﻿namespace API.VisionAuditiva.Models
+{
+    public class Login
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
