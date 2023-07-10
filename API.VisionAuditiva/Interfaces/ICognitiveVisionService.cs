0a190f101bae50b525a708c5d6456b3ab6ccd18b
xstefano <xcloudsgls@gmail.com>
2023-07-09 22:13:17 -0600

    implementing the VisionAuditiva class

---
+++ API.VisionAuditiva/Interfaces/ICognitiveVisionService.cs
@@ -0,0 +1,10 @@
+﻿namespace API.VisionAuditiva.Interfaces
+{
+    public interface ICognitiveVisionService
+    {
+        public Task<string> AnalyzeFromUrlAsync(string imageUrl);
+        public Task<string> DescribeFromUrlAsync(string imageUrl);
+        public Task<string> ReadImageUrlAsync(string imageUrl);
+        public Task<string> DetectObjectsFromUrlAsync(string imageUrl);
+    }
+}
